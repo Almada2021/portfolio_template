a27dba9ea257a28a0dde607aeaa1e7c1491ac6f2
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2023-04-27 02:54:07 +0000

    correct another link

--- portfolio/src/ListProyects/listProyects.js
+++ portfolio/src/ListProyects/listProyects.js
@@ -39,7 +39,7 @@ export const listProyect = [
         title: "Cart of Ecommerce",
         description: textEcommerce,
         img: ecommerce,
-        deploy: "https://quiet-douhua-6d1099.netlify.app/store",
+        deploy: "https://quiet-douhua-6d1099.netlify.app/",
         github: "https://github.com/Almada2021/MUI-Project"
     }
 ]
